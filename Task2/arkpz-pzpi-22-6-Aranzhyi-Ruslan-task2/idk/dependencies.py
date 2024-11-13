cefc53761986be3ba54684ea27906819e7c412c7
Ruslan <ruslan.aranzhyi@nure.ua>
2024-11-13 17:36:32 +0200

    task2: implement measurements adding

--- Task2/arkpz-pzpi-22-6-Aranzhyi-Ruslan-task2/idk/dependencies.py
+++ Task2/arkpz-pzpi-22-6-Aranzhyi-Ruslan-task2/idk/dependencies.py
@@ -7,20 +7,18 @@ from idk.models.user import UserRole, User
 from idk.utils.custom_exception import CustomMessageException
 
 
-class JWTAuthSession:
-    async def __call__(
-            self,
-            authorization: str | None = Header(default=None),
-            x_token: str | None = Header(default=None),
-    ) -> Session:
-        authorization = authorization or x_token
-        if not authorization or (session := await Session.from_jwt(authorization)) is None:
-            raise CustomMessageException("Invalid session.", 401)
+async def jwt_auth_session(
+        authorization: str | None = Header(default=None),
+        x_token: str | None = Header(default=None),
+) -> Session:
+    authorization = authorization or x_token
+    if not authorization or (session := await Session.from_jwt(authorization)) is None:
+        raise CustomMessageException("Invalid session.", 401)
 
-        return session
+    return session
 
 
-JwtSessionDep = Annotated[Session, Depends(JWTAuthSession())]
+JwtSessionDep = Annotated[Session, Depends(jwt_auth_session)]
 
 
 class JWTAuthUser:
@@ -45,3 +43,27 @@ async def sensor_dep(user: JwtAuthUserDep, sensor_id: int) -> Sensor:
 
 
 SensorDep = Annotated[Sensor, Depends(sensor_dep)]
+
+
+async def sensor_auth(
+        authorization: str | None = Header(default=None),
+        x_token: str | None = Header(default=None),
+) -> Sensor:
+    authorization = authorization or x_token
+    authorization = authorization.split(":")
+    if len(authorization) != 3:
+        raise CustomMessageException("Unknown sensor.", 404)
+    sensor_key = authorization[2]
+    try:
+        user_id = int(authorization[0])
+        sensor_id = int(authorization[1])
+    except ValueError:
+        raise CustomMessageException("Unknown sensor.", 404)
+
+    if (sensor := await Sensor.get_or_none(id=sensor_id, owner__id=user_id, secret_key=sensor_key)) is None:
+        raise CustomMessageException("Unknown sensor.", 404)
+
+    return sensor
+
+
+SensorAuthDep = Annotated[Sensor, Depends(sensor_auth)]
--- Task2/arkpz-pzpi-22-6-Aranzhyi-Ruslan-task2/idk/routers/measurements.py
+++ Task2/arkpz-pzpi-22-6-Aranzhyi-Ruslan-task2/idk/routers/measurements.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
 
+from idk.dependencies import SensorAuthDep
+from idk.models import Measurement
+from idk.schemas.measurements import AddMeasurementRequest
+
 router = APIRouter(prefix="/measurements")
+
+
+@router.post("", status_code=204)
+async def add_measurement(sensor: SensorAuthDep, data: AddMeasurementRequest):
+    await Measurement.create(sensor=sensor, **data.model_dump())
